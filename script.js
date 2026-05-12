8ffcf1b8e93e6680666a5f3da9dfda131df9f9e2
pavelcorcevschi <korcevskijpavel6@gmail.com>
2026-05-12 12:10:31 +0300

    ep1-7

--- script.js
+++ script.js
@@ -6,4 +6,33 @@ function celsiusToFahrenheit(c) {
 console.log(celsiusToFahrenheit(0));
 console.log(celsiusToFahrenheit(25));
 
+// 2
+function isPrime(n) {
+    if (n <= 1) return false;
+
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime(7));
+console.log(isPrime(10));
+
+
+// 3
+function doubleArray(arr) {
+    let res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        res.push(arr[i] * 2);
+    }
+
+    return res;
+}
+
+console.log(doubleArray([1,2,3]));
+
 
